748536e1ad3b7999aaf06070c959a05c4f67cc16
ankit1200 <ankit1200@Ankits-MacBook-Pro.local>
2014-08-28 16:42:15 -0400

    added parse framework

--- VT Transit/AppDelegate.swift
+++ VT Transit/AppDelegate.swift
@@ -17,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
         // Override point for customization after application launch.
         
+        Parse.setApplicationId("DYUHcRr8CGd2KgBCiZzmoQZGUHs4mEvYOFDE3ecF", clientKey: "WanDsaJNb5ofp7cv0r6hQK87KEwPqiaCrcPmORI6")
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
         return true
     }
 
